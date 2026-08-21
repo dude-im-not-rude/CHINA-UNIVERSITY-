dd7be19c24799261f4a7f3016dcb433d6c326c17
dude-im-not-rude <dud91260@gmail.com>
2026-08-21 23:26:04 +0530

    Hide public UI on admin routes

--- app/components/global-ui.js
+++ app/components/global-ui.js
@@ -1,3 +1,6 @@
+"use client";
+
+import { usePathname } from "next/navigation";
 import CookieBanner from "./cookie-banner";
 import ContactWidget from "./contact-widget";
 import SiteFooter from "./site-footer";
@@ -5,5 +8,7 @@ import MobileNav from "./mobile-nav";
 import SiteControls from "./site-controls";
 
 export default function GlobalUI() {
+  const pathname = usePathname();
+  if (pathname?.startsWith("/admin")) return null;
   return <><MobileNav /><SiteControls /><SiteFooter /><ContactWidget /><CookieBanner /></>;
 }
