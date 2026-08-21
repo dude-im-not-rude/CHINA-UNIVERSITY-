81b1914c482300d10cdfc85937052f8103f16b96
dude-im-not-rude <dud91260@gmail.com>
2026-08-21 18:45:07 +0530

    Add global site UI layer

---
+++ app/components/global-ui.js
@@ -0,0 +1,7 @@
+import CookieBanner from "./cookie-banner";
+import ContactWidget from "./contact-widget";
+import SiteFooter from "./site-footer";
+
+export default function GlobalUI() {
+  return <><SiteFooter /><ContactWidget /><CookieBanner /></>;
+}
