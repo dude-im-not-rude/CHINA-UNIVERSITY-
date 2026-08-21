baf28104c6ef03863507eab08e87ca132cad511d
dude-im-not-rude <dud91260@gmail.com>
2026-08-22 00:12:08 +0530

    Add private Ashu admin entry route

---
+++ app/ashu/page.js
@@ -0,0 +1,7 @@
+import AdminLogin from "../admin/admin-login";
+
+export const metadata={title:"Private Control Center | ChinaUniTracker"};
+
+export default function AshuAdminEntry(){
+  return <AdminLogin/>;
+}
