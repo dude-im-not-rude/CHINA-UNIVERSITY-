40af7738938e9e6f79fc06b5393ee909f80e3c22
dude-im-not-rude <dud91260@gmail.com>
2026-08-21 18:03:24 +0530

    Add root layout

---
+++ app/layout.js
@@ -0,0 +1,3 @@
+import "./globals.css";
+export const metadata={title:"ChinaUniTracker",description:"China university, scholarship and CSCA discovery platform"};
+export default function RootLayout({children}){return <html lang="en"><body>{children}</body></html>}
