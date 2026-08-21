c194441095f5c5d6ab187456636ce9e89c006fd8
dude-im-not-rude <dud91260@gmail.com>
2026-08-21 21:27:21 +0530

    Add global theme and language controls

--- app/components/global-ui.js
+++ app/components/global-ui.js
@@ -2,7 +2,8 @@ import CookieBanner from "./cookie-banner";
 import ContactWidget from "./contact-widget";
 import SiteFooter from "./site-footer";
 import MobileNav from "./mobile-nav";
+import SiteControls from "./site-controls";
 
 export default function GlobalUI() {
-  return <><MobileNav /><SiteFooter /><ContactWidget /><CookieBanner /></>;
+  return <><MobileNav /><SiteControls /><SiteFooter /><ContactWidget /><CookieBanner /></>;
 }
